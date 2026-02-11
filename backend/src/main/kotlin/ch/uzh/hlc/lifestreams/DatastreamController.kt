6cc86321026f24be616f21febe09c92d1670759f
Arooran Thanabalasingam <13069024+bigaru@users.noreply.github.com>
2026-02-11 09:24:05 +0100

    chore: remove TestController

--- backend/src/main/kotlin/ch/uzh/hlc/lifestreams/DatastreamController.kt
+++ backend/src/main/kotlin/ch/uzh/hlc/lifestreams/DatastreamController.kt
@@ -5,21 +5,27 @@ import ch.uzh.hlc.lifestreams.model.DatastreamRepository
 import kotlinx.coroutines.flow.Flow
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
 import java.util.*
 
 
 @RestController
 @RequestMapping("/api/v1/datastreams")
 class DatastreamController(
+	@Value($$"${spring.application.name}") val appName: String,
 	private val repo: DatastreamRepository,
 ) {
 	companion object {
 		val log: Logger = LoggerFactory.getLogger(DatastreamController::class.java)
 	}
+
+	@GetMapping("/")
+	fun index(): Mono<String> = Mono.just("Hello  $appName")
 
 	// @TODO retrieve individualId from auth
 	@GetMapping("/overview/{individualId}")
