b9497c8ed28d670d9bba373ce4f8ae7586db6a32
Arooran Thanabalasingam <13069024+bigaru@users.noreply.github.com>
2026-02-18 20:39:17 +0100

    feat: handle pan, pinch and tooltip

--- mobile/app/datastreams/[id].tsx
+++ mobile/app/datastreams/[id].tsx
@@ -1,48 +1,75 @@
 import { Stack, useLocalSearchParams } from 'expo-router'
 import { H3, YStack, View, XStack } from 'tamagui'
-import { CartesianChart, Line, useChartPressState, useChartTransformState } from 'victory-native'
+import { CartesianChart, getTransformComponents, Line, useChartPressState, useChartTransformState } from 'victory-native'
 import { Circle, useFont } from '@shopify/react-native-skia'
-import type { SharedValue } from 'react-native-reanimated'
+import { useDerivedValue, type SharedValue } from 'react-native-reanimated'
 import inter from '../../assets/inter-medium.ttf'
-import { Line as SkiaLine } from '@shopify/react-native-skia'
+import { Line as SkiaLine, Text as SkiaText } from '@shopify/react-native-skia'
+import { useState } from 'react'
 
 const DATA = Array.from({ length: 61 }, (_, i) => ({
 	day: i,
 	highTmp: 40 + 30 * Math.random(),
 }))
 
-const initialViewport: any = { x: [45, 60] }
+const BASE_STROKE = 2
+
+function ToolTip({ x, y }: { x: SharedValue<number>; y: SharedValue<number> }) {
+	return <Circle cx={x} cy={y} r={6} color="black" />
+}
 
 export default function () {
 	const { id } = useLocalSearchParams<{ id: string }>()
 	const font = useFont(inter, 12)
+	const [chartTop, setChartTop] = useState(0)
+	const [chartBottom, setChartBottom] = useState(0)
 
 	const { state, isActive } = useChartPressState({ x: 0, y: { highTmp: 0 } })
 	const { state: transformState } = useChartTransformState()
+
+	const verticalLine_p1 = useDerivedValue(() => ({ x: state.x.position.value, y: chartTop }))
+	const verticalLine_p2 = useDerivedValue(() => ({ x: state.x.position.value, y: chartBottom }))
+
+	const strokeW = useDerivedValue(() => {
+		const vals = getTransformComponents(transformState.matrix.value)
+		const sx = vals.scaleX ?? 1
+		return BASE_STROKE / sx
+	})
+	const strokeWHelpLine = useDerivedValue(() => {
+		const vals = getTransformComponents(transformState.matrix.value)
+		const sx = vals.scaleX ?? 1
+		return 1 / sx
+	})
 
 	return (
 		<>
 			<Stack.Screen options={{ title: `Datastreams ${id}` }} />
 			<YStack items="center" justify="center" flex={1}>
-				<View my="$10" flex={1} width="100%">
+				<View my="$10" flex={1} width="90%">
 					<CartesianChart
 						data={DATA}
 						xKey="day"
 						yKeys={['highTmp']}
 						yAxis={[{ font: font, enableRescaling: true }]}
 						xAxis={{ font: font, enableRescaling: true }}
-						padding={12}
 						axisOptions={{ font }}
 						chartPressState={state}
-						transformConfig={{ pan: { enabled: true, activateAfterLongPress: 100 }, pinch: { enabled: true } }}
+						transformConfig={{ pan: { enabled: true, dimensions: ['x'] }, pinch: { enabled: true, dimensions: ['x'] } }}
 						transformState={transformState}
-						viewport={initialViewport}
+						gestureLongPressDelay={200}
+						onChartBoundsChange={({ top, bottom }) => {
+							setChartTop(top)
+							setChartBottom(bottom)
+						}}
 					>
-						{({ points, chartBounds }) => (
+						{({ points }) => (
 							<>
-								<Line points={points.highTmp} color="red" strokeWidth={3} />
+								<Line points={points.highTmp} color="red" strokeWidth={strokeW} />
 								{isActive && (
-									<SkiaLine p1={{ x: state.x.position.value, y: chartBounds.top }} p2={{ x: state.x.position.value, y: chartBounds.bottom }} strokeWidth={1} />
+									<>
+										<SkiaLine p1={verticalLine_p1} p2={verticalLine_p2} strokeWidth={strokeWHelpLine} />
+										<ToolTip x={state.x.position} y={state.y.highTmp.position} />
+									</>
 								)}
 							</>
 						)}
