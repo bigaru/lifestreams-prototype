911e2c671069a8d2c124723c2895fde0e32d3c4f
Arooran Thanabalasingam <13069024+bigaru@users.noreply.github.com>
2026-03-04 22:53:57 +0100

    feat: optimize base layout

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,51 +1,84 @@
 import { TamaguiProvider, Theme } from '@tamagui/core'
-import { ChartPie, Search, Settings, UserCircle2 } from '@tamagui/lucide-icons'
+import { ChartPie, Folder, HelpCircle, Info, Search, Settings, UserCircle2 } from '@tamagui/lucide-icons'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router'
-import { Button, Heading, SizableText, XStack, YStack } from 'tamagui'
+import { BrowserRouter, Link, Route, Routes, useLocation } from 'react-router'
+import { Heading, ListItem, XGroup, XStack, YGroup, YStack } from 'tamagui'
 import { config } from '../tamagui.config.ts'
-import App from './App.tsx'
+import DataTypes from './DataTypes.tsx'
 
 const menuItems = [
-	{ title: 'Data Discovery', icon: <Search size="$1" strokeWidth={1} /> },
-	{ title: 'Data Availability', icon: <ChartPie size="$1" strokeWidth={1} /> },
+	{ path: '/data-types', title: 'Discover Data Types', icon: Search },
+	{ path: '/', title: 'Data Availability', icon: ChartPie },
+	{ path: '/', title: 'Projects', icon: Folder },
+	{ path: '/', title: 'Documentation', icon: Info },
 ]
+const bottomMenuItems = [{ path: '/', title: 'Help', icon: HelpCircle }]
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<TamaguiProvider config={config}>
-			<Theme name={'light'}>
-				<XStack flex={1} items={'center'} width={'100%'} bg="$background" height="100vh">
-					<YStack p="$2" borderRightWidth="$0.25" borderColor="#dddddd" height="100%">
-						<Heading size="$6" fontWeight="600" p="$5">
-							LIFEstreams
-						</Heading>
-						{menuItems.map((m) => (
-							<Button chromeless justify={'flex-start'} width="100%" px="$4" borderWidth={0} py={0}>
-								<Button.Icon>{m.icon}</Button.Icon>
-								<Button.Text>
-									<SizableText size="$4" fontWeight="600" color="$black8">
-										{m.title}
-									</SizableText>
-								</Button.Text>
-							</Button>
-						))}
-					</YStack>
-					<YStack flex={1} height="100%">
-						<XStack borderBottomWidth="$0.25" borderColor="#dddddd" justify="flex-end">
-							<Settings size="$2" mx="$2.5" my="$2.5" strokeWidth={1} />
-							<UserCircle2 size="$2" mx="$2.5" my="$2.5" mr="$4" strokeWidth={1} />
-						</XStack>
-						<BrowserRouter>
-							<Routes>
-								<Route index element={<App />} />
-								<Route path="about" element={<div>Fuguuuu</div>} />
-							</Routes>
-						</BrowserRouter>
-					</YStack>
-				</XStack>
-			</Theme>
+			<BrowserRouter>
+				<Shell />
+			</BrowserRouter>
 		</TamaguiProvider>
 	</StrictMode>
 )
+
+function Shell() {
+	const { pathname } = useLocation()
+
+	return (
+		<Theme name={'light'}>
+			<XStack flex={1} items={'center'} width={'100%'} bg="$background" height="100vh">
+				<YStack p="$2" pb="$3" borderRightWidth="$0.25" borderColor="#dddddd" height="100vh">
+					<Heading size="$5" fontWeight="500" pt="$1" px="$4" pb="$6">
+						LIFEstreams
+					</Heading>
+					<YGroup flex={1}>
+						{menuItems.map((m) => (
+							<YGroup.Item key={m.title}>
+								<Link to={m.path} style={{ textDecoration: 'none' }}>
+									<ListItem
+										bg={pathname !== '/' && pathname === m.path ? '$blue5' : 'transparent'}
+										style={{ borderRadius: 5 }}
+										icon={m.icon}
+										minH={0}
+										py="$1.5"
+										hoverStyle={{ background: '#eee' }}
+									>
+										{m.title}
+									</ListItem>
+								</Link>
+							</YGroup.Item>
+						))}
+					</YGroup>
+					<YGroup>
+						{bottomMenuItems.map((m) => (
+							<YGroup.Item key={m.title}>
+								<ListItem bg={'transparent'} style={{ borderRadius: 5 }} icon={m.icon} minH={0} py="$1.5" hoverStyle={{ background: '#eee' }}>
+									{m.title}
+								</ListItem>
+							</YGroup.Item>
+						))}
+					</YGroup>
+				</YStack>
+				<YStack flex={1} height="100%">
+					<XGroup style={{ borderRadius: 0 }} borderBottomWidth="$0.25" borderColor="#dddddd" justify="flex-end">
+						<XGroup.Item>
+							<Settings size="$2" mx="$2" my="$2" strokeWidth={1} />
+						</XGroup.Item>
+						<XGroup.Item>
+							<UserCircle2 size="$2" mx="$2" my="$2" mr="$4" strokeWidth={1} />
+						</XGroup.Item>
+					</XGroup>
+
+					<Routes>
+						<Route index element={<></>} />
+						<Route path="/data-types" element={<DataTypes />} />
+					</Routes>
+				</YStack>
+			</XStack>
+		</Theme>
+	)
+}
