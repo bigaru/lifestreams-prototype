ab2edd97cd59b37aff7979c4e7c447135f572a95
Arooran Thanabalasingam <13069024+bigaru@users.noreply.github.com>
2026-03-12 01:50:44 +0100

    chore: add last 13 months hr data

--- backend/db/init/03_import_health_data.sql
+++ backend/db/init/03_import_health_data.sql
@@ -4,17 +4,34 @@ LANGUAGE plpython3u
 AS $$
 import pandas as pd
 
-df = pd.read_json('/docker-entrypoint-initdb.d/data/hr1.json')
+upper_bound = 3
+hrs = []
 
-# Milliseconds to seconds
-df[0] = df[0]/1000.0
+for h in range(1,upper_bound+1):
+	df = pd.read_json(f"/docker-entrypoint-initdb.d/data/hr{h}.json")
+	df[0] = pd.to_datetime(df[0], unit='ms')
+	hrs.append(df)
+
+today = pd.Timestamp.today().normalize()
+start_date = today - pd.DateOffset(months=13)
+past_dates = pd.date_range(start=start_date, end=today, freq="D", tz="Europe/Zurich")
+dfs = []
+
+for day in past_dates:
+	idx = day.weekday() % upper_bound
+	hr_df = hrs[idx].copy()
+	hr_df[0] = day + (hr_df[0] - hr_df[0].dt.normalize())
+	dfs.append(hr_df)
+
+final_df = pd.concat(dfs, ignore_index=True)
+final_df[0] = final_df[0].dt.to_pydatetime()
 
 plan = plpy.prepare(
-    "INSERT INTO datastreams (individual_category_id, created_at, value) VALUES ($1, to_timestamp($2), $3)",
-    ["bigint", "double precision", "double precision"]
+    "INSERT INTO datastreams (individual_category_id, created_at, value) VALUES ($1, $2, $3)",
+    ["bigint", "timestamptz", "double precision"]
 )
 
-for row in df.itertuples(index=False):
+for row in final_df.itertuples(index=False):
     plpy.execute(plan, [1, row[0], row[1]])
 $$;
 
